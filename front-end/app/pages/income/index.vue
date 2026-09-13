6f6843c3811f4829163f70692d3c10288bc0a3cf
ThearaKong <theara.kong@khawin.com>
2026-09-13 19:49:45 +0700

    Change income

--- front-end/app/pages/income/index.vue
+++ front-end/app/pages/income/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="income">
-        <h1 class="text-2xl font-bold mb-4">Income</h1>
+        <h1 class="text-2xl font-bold mb-4">Income money</h1>
         
     </div>
 </template>
