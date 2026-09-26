c5b7ba4ba3d24691029651a00d5d1f3d045fd4eb
ThearaKong <theara.kong@khawin.com>
2026-09-26 16:06:38 +0700

    set new bcrypt

--- back-end/core/config.py
+++ back-end/core/config.py
@@ -1,19 +1,19 @@
 
 import os
-from typing import List, Optional
+from typing import List
 
 
 from pydantic import field_validator
 from pydantic_settings import BaseSettings
 
 class Setting(BaseSettings):
-    POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "123456"
-    POSTGRES_HOST: str = "postgres-service"
-    POSTGRES_PORT: str = "5432"
-    POSTGRES_DB: str = "erp_db"
-    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    ALGORITHM: str = "HS256"
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "123456")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "postgres-service") 
+    POSTGRES_PORT: str =os.getenv("POSTGRES_PORT", "5432") 
+    POSTGRES_DB: str =os.getenv("POSTGRES_DB", "erp_db") 
+    SECRET_KEY: str =os.getenv("SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
+    ALGORITHM: str =os.getenv("ALGORITHM", "HS256")
     
     @property
     def database_url(self) -> str:
